df818875ab3a30910df7493189dbcbb7fb095864
aalii00 <167920464+aalii00@users.noreply.github.com>
2026-05-05 11:33:25 +0300

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -1,4 +1,20 @@
 public class Operations {
   public static void main(String[] args){
+    displaySquare(5);
+    displayIsEven(8);
+    
   }
+  static void displaySquare(int n) {
+        System.out.println(n * n);
+  }
+    static void displayIsEven(int n) {
+        if (n % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+  }
+    
+
+    
 }
