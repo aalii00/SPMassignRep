77ead3fb2347710350b0003c013a98c38b1ee358
aalii00 <167920464+aalii00@users.noreply.github.com>
2026-05-10 21:27:10 +0300

    Update Operations.java
    change

--- Operations.java
+++ Operations.java
@@ -1,6 +1,9 @@
 public class Operations {
   public static void main(String[] args) {
 
+    System.out.println(sayHello("elan"));
+    System.out.println(doubleNumber(25));
+
     displaySquare(5);
     displayIsEven(8);
 
@@ -31,7 +34,12 @@ public class Operations {
   public static int min_to_hour(int minutes) {
     return minutes / 60;
   }
+
+  public static String sayHello(String name) {
+    return "Hello, " + name + "!";
 }
+  public static int doubleNumber(int n) {
+    return n * 2;}
 
-
+}
 
