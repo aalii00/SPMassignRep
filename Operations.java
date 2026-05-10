d01e0e2775d3f1d9c762ed197de794e21b3ef2df
elanalq <202200406@pmu.edu.sa>
2026-05-10 20:24:42 +0300

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -2,7 +2,17 @@ public class Operations {
   public static void main(String[] args){
    
   }
+public static int calculateSum(int a, int b) {
+    return a + b;
+}
 
+public static int getMaxValue(int a, int b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
   
 }
 
