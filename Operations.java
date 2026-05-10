25124baf5742ffcbe2fa109f9b9cbc6769a73128
aalii00 <167920464+aalii00@users.noreply.github.com>
2026-05-10 14:01:45 +0300

    Update Operations.java
    braces issues

--- Operations.java
+++ Operations.java
@@ -1,14 +1,14 @@
 public class Operations {
-  public static void main(String[] args){
-    int total = calculateSum(5, 3);   
-    int max = getMaxValue(5, 3);
+    public static void main(String[] args) {
+        int total = calculateSum(5, 3);   
+        int max = getMaxValue(5, 3);
+    } 
 
-  }
+    public static int calculateSum(int a, int b) {
+        return a + b;
+    }
 
-public static int calculateSum(int a, int b) {
-    return a + b;
-}
-public static int getMaxValue(int a, int b) {
-    return Math.max(a, b);
-}
-}
+    public static int getMaxValue(int a, int b) {
+        return Math.max(a, b);
+    }
+} // hope it works
