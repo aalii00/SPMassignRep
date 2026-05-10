1bc02eb40d66533be6238457a3044b5dc18fa512
aljorykhalid <202101727@pmu.edu.sa>
2026-05-10 13:17:55 +0300

    Update Operations.java
    comment 3

--- Operations.java
+++ Operations.java
@@ -1,4 +1,9 @@
-public class Operations {
-  public static void main(String[] args){
-  }
+public static int calculateSum(int a, int b) {
+    return a + b;
 }
+public static int getMaxValue(int a, int b) {
+    return Math.max(a, b);
+}
+
+int total = calculateSum(5, 3);   
+int max = getMaxValue(5, 3);
