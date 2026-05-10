3bc25583e4ac89da13aab0d4c17ca66f092ebc2b
aalii00 <167920464+aalii00@users.noreply.github.com>
2026-05-10 14:37:05 +0300

    Update Operations.java
    edits

--- Operations.java
+++ Operations.java
@@ -1,14 +1,18 @@
 public class Operations {
   public static void main(String[] args){
-    int total = calculateSum(5, 3);   
-    int max = getMaxValue(5, 3); 
+    displaySquare(5);
+    displayIsEven(8); 
   }
-public static int calculateSum(int a, int b) {
-    return a + b;
-}
-public static int getMaxValue(int a, int b) {
-    return Math.max(a, b);
-}
+static void displaySquare(int n) {
+        System.out.println(n * n);
+  }
+    static void displayIsEven(int n) {
+        if (n % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+  }
   
 }
 
