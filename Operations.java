8a73ff12298a3be157cc3fc504c6bd568fb0fc8e
aalii00 <167920464+aalii00@users.noreply.github.com>
2026-05-10 21:06:43 +0300

    Update Operations.java
    elan did her part

--- Operations.java
+++ Operations.java
@@ -1,8 +1,21 @@
 public class Operations {
   public static void main(String[] args){
-   
-  }
+    int k = 10;
+    int m = 3;
+    System.out.println(subtract(k,m));
 
+    int minutes = 120;
+    System.out.println(convertToHours(minutes));
+
+
+    
+  }
+public static int subtract(int x, int y) {
+    return x - y;
+  }
+  public static int min_to_hour(int minutes) {
+    return minutes / 60;
+  }
   
 }
 
